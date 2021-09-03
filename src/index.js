51bf20d6267689cc40ba2a09347a9f219674b210
mykhailovskyiv <56435226+mykhailovskyiv@users.noreply.github.com>
2021-09-03 12:45:41 +0300

    virna

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import 'normalize.css';
 require('./style.scss')
 require('./assets/fonts/CeraPro-Regular.woff2')
 require('./assets/fonts/CeraPro-Regular.woff')
+require('./assets/img/arrow.svg')
+require('./assets/img/header.png')
+require('./assets/img/icon_camera.svg')
+require('./assets/img/icon_cart.svg')
+require('./assets/img/icon_like.svg')
+require('./assets/img/logo.svg')
+require('./assets/img/logoVirna.svg')
+require('./assets/img/mobileHeader.png')
 document.querySelector('.c-hamburger').addEventListener('click', function (e) {
     let mobileMenu = document.getElementById('mobile-menu');
     e.preventDefault();
@@ -14,3 +22,7 @@ document.querySelector('.c-hamburger').addEventListener('click', function (e) {
     }
 
 })
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
